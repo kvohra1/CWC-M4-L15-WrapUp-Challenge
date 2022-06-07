78e5d0c78604ec4a41aaade301e4f70cb0acc511
Lightwand <karlavohra@yahoo.com>
2022-06-07 14:20:24 -0700

    Initial Commit

---
+++ MyLibrary/MyLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLibraryApp.swift
+//  MyLibrary
+//
+//  Created by Karla Vohra on 6/7/22.
+//
+
+import SwiftUI
+
+@main
+struct MyLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
